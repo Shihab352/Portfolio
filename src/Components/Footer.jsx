b3978f6b06f61e4c8c9a5f0e2934595951b7ae89
Sazzadul karim <sazzadulkarim352@gmail.com>
2024-09-14 11:49:36 +0600

    Footer section updated

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,13 +5,13 @@ import { FaLinkedin } from "react-icons/fa";
 const Footer =() => {
     return (
         <div>
-            <footer className="bg-slate-500 text-center p-6 flex items-center justify-evenly">
-      <div className="flex justify-center space-x-4">
+            <footer className="bg-slate-500 text-center p-6 lg:flex items-center justify-evenly">
+      <div className="lg:flex justify-center gap-2 space-x-4">
         {/* <a href="https://github.com" className="text-gray-600 hover:text-primary">GitHub</a>
         <a href="https://linkedin.com" className="text-gray-600 hover:text-primary">LinkedIn</a> */}
          {/* Email Me Button with Tooltip */}
          <div className="tooltip" data-tip="Sazzadulkarim352@gmail.com">
-          <a href="mailto:email@example.com" className="btn text-lg font-lato text-white 
+          <a href="mailto:email@example.com" className="btn  text-lg font-lato text-white 
     bg-gradient-to-r from-slate-700 from-10% via-slate-900 via-30% to-black to-90%  hover:from-black  hover:to-slate-900  ">
           <MdEmail />
             Email me
